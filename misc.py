b7f1b9a1035354fda439f466da4ba9cb987d12ec
MrClottom <phildumonet@gmail.com>
2019-08-01 02:03:30 +0200

    Added spacing

--- misc.py
+++ misc.py
@@ -1,14 +1,18 @@
 def confirm(bit_arr, val):
     return int(''.join(str(b) for b in bit_arr), 2) == int(val[1:], 16)
 
+
 def check_str_range(start, end):
     return lambda s: s.isnumeric() and start <= int(s) <= end
+
 
 def ar(*args, **kwargs):
     return (args, kwargs)
 
+
 def kw(**kwargs):
     return kwargs
+
 
 def extract_val(val, *attrs):
     for attr in attrs:
