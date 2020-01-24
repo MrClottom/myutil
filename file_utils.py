99b4da2677519cb47f5b7c2ca56bff8444a357e1
MrClottom <phildumonet@gmail.com>
2020-01-24 19:23:36 +0100

    Added stuff

--- file_utils.py
+++ file_utils.py
@@ -1,7 +1,7 @@
-from os import path
-from os import listdir
+from os import path, listdir, makedirs, remove, rmdir
 import json
-from base64 import b64encode
+from base64 import b64encode, b64decode
+from zlib import compress as zc, decompress as zd
 
 
 def is_ext(file_name, ext, verbose=False):
@@ -21,24 +21,74 @@ def file_to_obj(file_path):
         }
 
 
-def dir_to_obj(dir_path, confirm_func=lambda path: True):
-    def sub_dir(sub_path):
-        whole_path = path.join(dir_path, sub_path)
-        if path.isdir(whole_path):
-            return dir_to_obj(whole_path)
-        return file_to_obj(whole_path)
+def path_to_obj(target_path, confirm_func=lambda tp: (lambda sub_path: True)):
+    if path.isdir(target_path):
+        return {
+            'name': path.split(target_path)[-1],
+            'sub_dirs': [
+                path_to_obj(path.join(target_path, sub_path),
+                            confirm_func=confirm_func)
+                for sub_path in filter(confirm_func(target_path),
+                                       listdir(target_path))
+            ]
+        }
+    elif path.isfile(target_path):
+        return file_to_obj(target_path)
+    else:
+        raise FileNotFoundError('file/folder \'{}\' not found'
+                                .format(target_path))
 
-    return {
-        'name': path.split(dir_path)[-1],
-        'sub_dirs': [
-            sub_dir(sub_path)
-            for sub_path in listdir(dir_path)
-            if confirm_func(path.join(dir_path, sub_path))
-        ]
-    }
+
+def obj_to_file(obj, target_path):
+    if 'sub_dirs' in obj and 'name' in obj:
+        dir_path = path.join(target_path, obj['name'])
+        makedirs(dir_path)
+        for sub_obj in obj['sub_dirs']:
+            obj_to_file(sub_obj, dir_path)
+    elif 'data' in obj and 'name' in obj:
+        with open(path.join(target_path, obj['name']), 'wb') as f:
+            f.write(obj['data'])
+    else:
+        raise ValueError('Invalid input obj must contain name and'
+                         + ' (sub_dirs/data) field')
 
 
 def obj_to_json(obj, default=lambda s: b64encode(s).decode(), **kwargs):
     return json.dumps(
         obj, default=default, **kwargs
     )
+
+
+def rec_remove(fp):
+    if path.isdir(fp):
+        for sub_path in listdir(fp):
+            rec_remove(path.join(fp, sub_path))
+        rmdir(fp)
+    else:
+        remove(fp)
+
+
+def compress_obj(data):
+    data = obj_to_json(data, default=lambda s: b64encode(zc(s, 9)).decode())
+    return zc(data.encode(), 9)
+
+
+def rec_decompress(obj, return_orig=False):
+    if isinstance(obj, dict):
+        return {
+            key: rec_decompress(value, key == 'name')
+            for key, value in obj.items()
+        }
+    if isinstance(obj, list):
+        return list(map(rec_decompress, obj))
+    if isinstance(obj, str):
+        if return_orig:
+            return obj
+        return zd(b64decode(obj.encode()))
+    raise TypeError('unrecognized type: {}'.format(type(obj)))
+
+
+def decompress_cobj(data):
+    print('data:', repr(data)[:200])
+    data = json.loads(zd(data).decode())
+    return rec_decompress(data)
--- optimization.py
+++ optimization.py
@@ -1,7 +1,9 @@
-import cProfile, pstats, io
+import cProfile
+import pstats
+import io
+
 
 def profile(fnc):
-
     """A decorator that uses cProfile to profile a function"""
     def inner(*args, **kwargs):
         pr = cProfile.Profile()
@@ -15,4 +17,33 @@ def profile(fnc):
         print(s.getvalue())
         return retval
 
+    return inner
+
+
+class ArgSaver:
+    __slots__ = ['args', 'kwargs']
+
+    def __init__(self):
+        self.args = None
+        self.kwargs = None
+
+    def __call__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        return self
+
+    def __iter__(self):
+        yield self.args
+        yield self.kwargs
+
+
+def tail_func(f):
+    def inner(*args, **kwargs):
+        res = ArgSaver()
+        mounted_f = f(res)
+        res = mounted_f(*args, **kwargs)
+        while isinstance(res, ArgSaver):
+            args, kwargs = res
+            res = mounted_f(*args, **kwargs)
+        return res
     return inner
